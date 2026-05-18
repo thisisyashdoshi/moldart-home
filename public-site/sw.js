90ee45cc08ea6e2bb1b7baef4e25ed021a0220a7
Moldart Automations <info@moldartindia.com>
2026-05-18 10:27:16 +0530

    feat(site): publish public website

---
+++ public-site/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
+  );
+});
+
+self.addEventListener('fetch', () => {
+  // Intentionally empty: this worker unregisters itself and never serves stale pages.
+});
